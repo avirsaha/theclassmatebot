843da148af70cf28b5478366368d057928617719
Aviraj Saha <aviraj.saha@outlook.com>
2023-09-20 13:26:46 +0530

    Resolved merge conflicts

--- src/main.py
+++ src/main.py
@@ -1,3 +1,25 @@
+# Copyright 2023 Aviraj Saha
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see https://www.gnu.org/licenses/gpl-3.0.txt.
+# ==============================================================================
+"""This is the main script of the bot"""
+
+
+__version__ = "0.1.0-alpha"
+__all__ = []
+
+import database_interface
 from typing import Final
 from os import getenv
 from datetime import datetime
@@ -14,7 +36,7 @@ from telegram.ext import (
 from dotenv import load_dotenv
 
 load_dotenv(
-    dotenv_path=".venv/.env"
+    dotenv_path=".env"
 )  # Create your own .env file at .venv before running this script.
 
 # Config for logging
@@ -35,7 +57,8 @@ BOT_USERNAME: Final[str] = getenv("username_telegrambot")
 
 # Commands
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text("This is what the bot says on start.")
+    with open("bot_responses/on_start.txt", "r") as file:
+        await update.message.reply_text(file.read())
 
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
